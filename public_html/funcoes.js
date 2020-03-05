f0e7f7b0240c063bc4238c0f8d0f267b11073d88
GabrielMarioto <camilomarioto@hotmail.com>
2020-03-05 08:17:16 -0300

    end

---
+++ public_html/funcoes.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function acao(obj)
+{
+    obj.style.color = "black";
+    obj.style.background = "yellow";
+    obj.style.width = "350px";
+    obj.style.heigth = "150px";
+    obj.style.transitionDuration = "0.5s";
+}
+
+function normal(obj)
+{
+    obj.style.color = "lightblue";
+    obj.style.background = "lightblue";
+    obj.style.width = "240px";
+    obj.style.heigth = "150px";
+}
